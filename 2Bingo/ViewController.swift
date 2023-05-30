2baebcb7ef3bafe2b537c35bb767a8919db4b09f
Ashika-18 <ashibasama@gmail.com>
2023-05-30 19:15:42 +0900

    Initial Commit

---
+++ 2Bingo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2Bingo
+//
+//  Created by Kazuhiro Ashiba on 2023/05/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
